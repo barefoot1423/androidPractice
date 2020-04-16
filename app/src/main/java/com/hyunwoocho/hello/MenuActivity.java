a27bda0cef8f29144f2114e7976194021f0dfe91
Hyunwoo Cho <barefoot1423@gmail.com>
2020-04-15 23:55:39 -0400

    clean up code

--- app/src/main/java/com/hyunwoocho/hello/MenuActivity.java
+++ app/src/main/java/com/hyunwoocho/hello/MenuActivity.java
@@ -7,15 +7,15 @@ import android.view.View;
 import android.widget.Toast;
 
 public class MenuActivity extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
     }
-
+    
     public void onBackButtonClicked(View view) {
-        Toast.makeText(getApplicationContext(), "Clicked back button", Toast.LENGTH_LONG).show();;
+        Toast.makeText(getApplicationContext(), "Clicked back button", Toast.LENGTH_LONG).show();
         finish();
     }
 }
